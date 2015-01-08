bfc882b2be3ba9fac3ef41f50da8a9cec0fda9b2
rssnsj <rssnsj@gmail.com>
2015-01-08 13:09:51 +0800

    shadowsocks-tools/: Enhancements.

--- shadowsocks-tools/files/etc/init.d/ss-redir.sh
+++ shadowsocks-tools/files/etc/init.d/ss-redir.sh
@@ -24,7 +24,7 @@ start()
 {
 	[ "$SS_DISABLED" = Y ] && return 1
 	if [ -z "$SS_SERVER_ADDR" -o -z "$SS_SERVER_PORT" ]; then
-		echo "*** Server address or port are not defined, aborted."
+		echo "WARNING: Shadowsocks server address is not defined, not starting it."
 		return 1
 	fi
 
