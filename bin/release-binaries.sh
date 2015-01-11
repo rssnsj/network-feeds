4896989fc13301b478ba9dcb5e4c26bf731df343
rssnsj <rssn@163.com>
2015-01-11 11:27:56 +0800

    release-binaries.sh: Adapt the fucking 'dash'.

--- bin/release-binaries.sh
+++ bin/release-binaries.sh
@@ -2,6 +2,17 @@
 
 mkdir -p ramips ar71xx
 
-cp -vf ~/openwrt-hc5761/openwrt-ramips/bin/ramips/packages/base/{dnsmasq_*.ipk,ipset-lists_*.ipk,kmod-proto-bridge_*.ipk,shadowsocks-libev_*.ipk,shadowsocks-tools_*.ipk} ramips/
-cp -vf ~/openwrt-hc6361/openwrt-ar71xx/bin/ar71xx/packages/base/{dnsmasq_*.ipk,ipset-lists_*.ipk,kmod-proto-bridge_*.ipk,shadowsocks-libev_*.ipk,shadowsocks-tools_*.ipk} ar71xx/
+cp -vf ~/openwrt-hc5761/openwrt-ramips/bin/ramips/packages/base/dnsmasq_*.ipk \
+	~/openwrt-hc5761/openwrt-ramips/bin/ramips/packages/base/ipset-lists_*.ipk \
+	~/openwrt-hc5761/openwrt-ramips/bin/ramips/packages/base/kmod-proto-bridge_*.ipk \
+	~/openwrt-hc5761/openwrt-ramips/bin/ramips/packages/base/shadowsocks-libev_*.ipk \
+	~/openwrt-hc5761/openwrt-ramips/bin/ramips/packages/base/shadowsocks-tools_*.ipk} \
+	ramips/
+
+cp -vf ~/openwrt-hc6361/openwrt-ar71xx/bin/ar71xx/packages/base/dnsmasq_*.ipk \
+	~/openwrt-hc6361/openwrt-ar71xx/bin/ar71xx/packages/base/ipset-lists_*.ipk \
+	~/openwrt-hc6361/openwrt-ar71xx/bin/ar71xx/packages/base/kmod-proto-bridge_*.ipk \
+	~/openwrt-hc6361/openwrt-ar71xx/bin/ar71xx/packages/base/shadowsocks-libev_*.ipk \
+	~/openwrt-hc6361/openwrt-ar71xx/bin/ar71xx/packages/base/shadowsocks-tools_*.ipk \
+	ar71xx/
 
