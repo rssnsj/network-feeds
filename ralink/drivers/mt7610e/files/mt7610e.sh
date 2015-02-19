181d62ca289b390f3bfa962499e77444bc39b611
rssnsj <rssnsj@gmail.com>
2015-02-19 16:56:39 +0800

    mt7610e.sh: Change the 5G name on initialization.

--- ralink/drivers/mt7610e/files/mt7610e.sh
+++ ralink/drivers/mt7610e/files/mt7610e.sh
@@ -21,30 +21,28 @@ enable_mt7610e() {
 
 detect_mt7610e() {
 #	detect_ralink_wifi mt7610e mt7610e
-	ssid=mt7610e-`ifconfig eth0 | grep HWaddr | cut -c 51- | sed 's/://g'`
+	#ssid=mt7610e-`ifconfig eth0 | grep HWaddr | cut -c 51- | sed 's/://g'`
 	cd /sys/module
 	[ -d $module ] || return
         [ -e /etc/config/wireless ] && return
          cat <<EOF
-config wifi-device      mt7610e
-        option type     mt7610e
-        option vendor   ralink
-        option band     5G
-        option channel  0
-		option autoch   2
+config wifi-device mt7610e
+	option type mt7610e
+	option vendor ralink
+	option band 5G
+	option channel 0
+	option autoch 2
 
 config wifi-iface
-        option device   mt7610e
-        option ifname   rai0
-        option network  lan
-        option mode     ap
-        option ssid     $ssid
-        option encryption psk2
-        option key      12345678
+	option device mt7610e
+	option ifname rai0
+	option network lan
+	option mode ap
+	option ssid OpenWrt-5G
+	option encryption none
 
 EOF
 
 
 }
-
 
