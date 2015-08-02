dcbffeff48bd0265ef3d26c1162f056100f6e35e
Justin Liu <rssnsj@gmail.com>
2015-08-02 22:18:45 +0800

    ipset-lists/: Supports generating route table.

--- ipset-lists/tools/gen-region-ipset.sh
+++ ipset-lists/tools/gen-region-ipset.sh
@@ -5,7 +5,12 @@
 #
 
 SOURCE_FILE_URL="http://ftp.apnic.net/stats/apnic/delegated-apnic-latest"
-OUTPUT_LIST_FILE=
+DATA_FILE=`basename "$SOURCE_FILE_URL"`
+
+check_data_file()
+{
+	[ -f $DATA_FILE ] || wget "$SOURCE_FILE_URL" -O $DATA_FILE || exit 1
+}
 
 # $1: country code in source file
 # $2: ipset table name
@@ -13,48 +18,51 @@ gen_ipset_to_stdout()
 {
 	local ctcode="$1"
 	local ctname="$2"
-	local tmpfile=`basename "$SOURCE_FILE_URL"`
 
-	[ -f $tmpfile  ] || wget "$SOURCE_FILE_URL" -O $tmpfile || return 1
+	check_data_file
 
 	echo "create $ctname hash:net family inet hashsize 1024 maxelem 65536"
 
-	cat $tmpfile | awk -F'|' -vc="$ctcode" '
+	cat $DATA_FILE | awk -F'|' -vc="$ctcode" '
 function tobits(c) {for(n=0;c>=2;c/=2){n++;};return 32-n;}
 $2==c&&$3=="ipv4"{printf("%s/%d\n",$4,tobits($5))}' |
 	awk -vt="$ctname" '{printf("add %s %s\n",t,$0)}'
 }
 
-gen_apnic_to_file()
+# $1: country code in source file
+# $2: ipset table name
+gen_rtable_to_stdout()
 {
 	local ctcode="$1"
-	local ctname="$2"
 
-	[ -n "$OUTPUT_LIST_FILE" ] || OUTPUT_LIST_FILE="../files/etc/ipset/$ctname"
+	check_data_file
 
-	if [ "$OUTPUT_LIST_FILE" = "-" ]; then
-		gen_ipset_to_stdout $ctcode "$ctname"
-	else
-		gen_ipset_to_stdout $ctcode "$ctname" > "$OUTPUT_LIST_FILE"
-	fi
+	cat $DATA_FILE | awk -F'|' -vc="$ctcode" '
+function tobits(c) {for(n=0;c>=2;c/=2){n++;};return 32-n;}
+$2==c&&$3=="ipv4"{printf("%s/%d\n",$4,tobits($5))}'
+
 }
 
 ##
-[ "$2" = "-o" ] && OUTPUT_LIST_FILE="$3" || :
+case "$2" in
+	-r) cmd=gen_rtable_to_stdout;;
+	*)  cmd=gen_ipset_to_stdout;;
+esac
+
 case "$1" in
-	CN) gen_apnic_to_file $1 china;;
-	TW) gen_apnic_to_file $1 taiwan;;
-	HK) gen_apnic_to_file $1 hongkong;;
-	SG) gen_apnic_to_file $1 singapore;;
-	JP) gen_apnic_to_file $1 japan;;
-	KR) gen_apnic_to_file $1 korea;;
+	CN) $cmd $1 china;;
+	TW) $cmd $1 taiwan;;
+	HK) $cmd $1 hongkong;;
+	SG) $cmd $1 singapore;;
+	JP) $cmd $1 japan;;
+	KR) $cmd $1 korea;;
 	*)
 		echo "Usage:"
-		echo "  $0 <country_code|carrier_name> [-o output_file]"
+		echo "  $0 <country_code> [-r]"
 		echo "Supported countries:"
 		echo "  CN, TW, HK, SG, JP, KR"
 		echo "Examples:"
-		echo "  $0 CN            written to ../files/etc/ipset/china"
-		echo "  $0 TW            written to ../files/etc/ipset/taiwan"
+		echo "  $0 CN > ../files/etc/ipset/china"
+		echo "  $0 TW > ../files/etc/ipset/taiwan"
 		;;
 esac
