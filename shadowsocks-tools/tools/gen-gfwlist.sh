7260e05a4cd075c53cfbeea1dcce2000adced808
Justin Liu <rssnsj@gmail.com>
2015-08-02 13:39:42 +0800

    shadowsocks-tools/: Revise text labels; updated gfwlist.

--- shadowsocks-tools/tools/gen-gfwlist.sh
+++ shadowsocks-tools/tools/gen-gfwlist.sh
@@ -2,15 +2,24 @@
 
 GFWLIST_URL="http://autoproxy-gfwlist.googlecode.com/svn/trunk/gfwlist.txt"
 
-[ -f gfwlist.txt ] || wget "$GFWLIST_URL"
+if [ ! -f gfwlist.txt ]; then
+	wget "$GFWLIST_URL" -O gfwlist.b64
+	cat gfwlist.b64 | base64 -d > gfwlist.txt
+	rm -f gfwlist.b64
+fi
 
-cat gfwlist.txt | base64 -d > gfwlist.1
-
-cat gfwlist.1 | 
+cat gfwlist.txt base-gfwlist.txt | sort -u | 
 	sed 's#!.\+##; s#|##g; s#@##g; s#http:\/\/##; s#https:\/\/##;' |
-	sed '/\*/d; /apple\.com/d; /sina\.cn/d; /sina\.com\.cn/d; /baidu\.com/d' |
+	sed '/\*/d; /apple\.com/d; /sina\.cn/d; /sina\.com\.cn/d; /baidu\.com/d; /qq\.com/d' |
 	sed '/^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+$/d' |
-	grep '^[0-9a-zA-Z\.-]\+$' | grep '\.' | sed 's#^\.\+##' | sort -u
-
-rm -f gfwlist.1
+	grep '^[0-9a-zA-Z\.-]\+$' | grep '\.' | sed 's#^\.\+##' | rev | sort -u |
+	awk '
+BEGIN { prev = "________"; }  {
+	cur = $0;
+	if (index(cur, prev) == 1 && substr(cur, 1 + length(prev) ,1) == ".") {
+	} else {
+		print cur;
+		prev = cur;
+	}
+}' | rev | sort -u
 
