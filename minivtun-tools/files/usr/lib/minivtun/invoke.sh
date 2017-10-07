0b4e78a3f040d9f6f1856550d802c5265e3a43d6
Justin Liu <rssnsj@gmail.com>
2017-10-07 16:12:17 +0800

    minivtun-tools/: Reuse the code of function 'do_pause'

--- minivtun-tools/files/usr/lib/minivtun/invoke.sh
+++ minivtun-tools/files/usr/lib/minivtun/invoke.sh
@@ -304,44 +304,6 @@ EOF
 
 }
 
-do_stop()
-{
-	local vt_network=`uci get minivtun.@minivtun[0].network 2>/dev/null`
-	local vt_proxy_mode=`uci get minivtun.@minivtun[0].proxy_mode 2>/dev/null`
-	[ -z "$vt_network" ] && vt_network="vt0"
-	local vt_ifname="minivtun-$vt_network"
-	local vt_gfwlist=`__gfwlist_by_mode $vt_proxy_mode`
-
-	# -----------------------------------------------------------------
-	rm -rf /var/etc/dnsmasq-go.d
-	if [ -f /tmp/dnsmasq.d/dnsmasq-go.conf ]; then
-		rm -f /tmp/dnsmasq.d/dnsmasq-go.conf
-		/etc/init.d/dnsmasq restart
-	fi
-
-	# -----------------------------------------------------------------
-	if iptables -t mangle -F minivtun_$vt_network 2>/dev/null; then
-		while iptables -t mangle -D OUTPUT -p udp --dport 53 -j minivtun_$vt_network 2>/dev/null; do :; done
-		while iptables -t mangle -D PREROUTING -j minivtun_$vt_network 2>/dev/null; do :; done
-		iptables -t mangle -X minivtun_$vt_network 2>/dev/null
-	fi
-
-	# -----------------------------------------------------------------
-	[ "$KEEP_GFWLIST" = Y ] || ipset destroy "$vt_gfwlist" 2>/dev/null
-
-	# -----------------------------------------------------------------
-	# We don't have to delete the default route in 'virtual', since
-	# it will be brought down along with the interface.
-	while ip rule del fwmark $VPN_ROUTE_FWMARK table $VPN_IPROUTE_TABLE 2>/dev/null; do :; done
-
-	ifdown $vt_network
-	if [ -f /var/run/$vt_ifname.pid ]; then
-		kill -9 `cat /var/run/$vt_ifname.pid`
-		rm -f /var/run/$vt_ifname.pid
-	fi
-
-}
-
 do_pause()
 {
 	local vt_network=`uci get minivtun.@minivtun[0].network 2>/dev/null`
@@ -373,6 +335,19 @@ do_pause()
 	while ip rule del fwmark $VPN_ROUTE_FWMARK table $VPN_IPROUTE_TABLE 2>/dev/null; do :; done
 
 	return 0
+}
+
+do_stop()
+{
+	do_pause
+
+	local vt_network=`uci get minivtun.@minivtun[0].network 2>/dev/null`
+	local vt_ifname="minivtun-$vt_network"
+	ifdown $vt_network
+	if [ -f /var/run/$vt_ifname.pid ]; then
+		kill -9 `cat /var/run/$vt_ifname.pid`
+		rm -f /var/run/$vt_ifname.pid
+	fi
 }
 
 #
